ab8b2e30be58f7d523fdd553c5cb2640423c02c3
Michael Sholty <michael@feathr.co>
2019-03-22 11:18:52 -0400

    add app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var currentPlayer = document.getElementById("currentPlayer");
 var currentPlayerPiece = document.getElementById("currentPlayerPiece");
 
+var winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6]];
+
 function checkWinner() {
     var cell0 = document.getElementById("0").className;
     var cell1 = document.getElementById("1").className;
